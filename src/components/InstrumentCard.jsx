6dcd93bb6a655f34163522846c16111cf58f398f
GaleArt <GaleguzovAA@gmail.com>
2024-03-17 18:18:55 +0300

    edit instrument elements style

--- src/components/InstrumentCard.jsx
+++ src/components/InstrumentCard.jsx
@@ -4,7 +4,7 @@ import { useRouter } from 'next/navigation';
 const InstrumentCard = ({ instrument }) => {
 	const router = useRouter();
 	return (
-		<div className='flex items-start w-full px-2 m-1 py-0.5 hover:bg-cyan-400 hover:cursor-pointer child:text-left rounded-lg'>
+		<div className='flex items-start w-full px-2 py-0.5 hover:bg-cyan-400 hover:cursor-pointer child:text-left rounded-lg even:bg-gray-300'>
 			<div className='w-1/12'>{instrument.smena}</div>
 			<div className='w-2/12'>{instrument.name}</div>
 			<div className='w-3/12'>{instrument.description}</div>
